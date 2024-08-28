b9705d48fbe2929e0ddb7e6820c15c727e629cbf
Ian Johnson <enjalot@gmail.com>
2024-08-28 15:19:05 -0400

    update deploy and config

--- web/next.config.mjs
+++ web/next.config.mjs
@@ -7,6 +7,8 @@ const nextConfig = {
   output: 'export',
   pageExtensions: ['js', 'jsx', 'ts', 'tsx', 'md', 'mdx'],
   transpilePackages: ['antd', '@ant-design', 'rc-util', 'rc-pagination', 'rc-picker'],
+  assetPrefix: process.env.NODE_ENV === 'production' ? '/latent-taxonomy/' : '',
+  basePath: process.env.NODE_ENV === 'production' ? '/latent-taxonomy' : '',
   webpack: (config) => {
     config.resolve.fallback = { fs: false, path: false };
     return config;
