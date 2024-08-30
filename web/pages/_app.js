d3babae45fc9e89d4e86e3510e5712664b6989a3
Ian Johnson <enjalot@gmail.com>
2024-08-30 11:03:17 -0400

    finish about page writeup. add GA.

--- web/pages/_app.js
+++ web/pages/_app.js
@@ -11,6 +11,22 @@ function MyApp({ Component, pageProps }) {
   return (<>
     <Head>
       <link rel="icon" href={`${basePath}/favicon.ico`} />
+      {process.env.NODE_ENV === 'production' && (
+            <>
+              <script async src="https://www.googletagmanager.com/gtag/js?id=G-42X5GRN7RD"></script>
+              <script
+                dangerouslySetInnerHTML={{
+                  __html: `
+                    window.dataLayer = window.dataLayer || [];
+                    function gtag(){dataLayer.push(arguments);}
+                    gtag('js', new Date());
+
+                    gtag('config', 'G-42X5GRN7RD');
+                  `,
+                }}
+              />
+            </>
+          )}
     </Head>
     <Component {...pageProps} />
   </>)
